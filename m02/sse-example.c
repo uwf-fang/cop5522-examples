bab601e4d68a3ee3199cf75a8f0ede3f2841996d
Ian Fang <xfang@uwf.edu>
2023-10-07 15:20:17 -0500

    add comments

--- m02/sse-example.c
+++ m02/sse-example.c
@@ -4,19 +4,20 @@
 // Function to perform SSE4-based vector matrix multiplication
 void vectorMatrixMultiply(const float* matrix, const float* inputVector,
                           float* resultVector) {
-  __m128 row1 = _mm_load_ps(matrix);        // Load the first row of the matrix
-  __m128 row2 = _mm_load_ps(matrix + 4);    // Load the second row
-  __m128 row3 = _mm_load_ps(matrix + 8);    // Load the third row
-  __m128 row4 = _mm_load_ps(matrix + 12);   // Load the fourth row
-  __m128 input = _mm_load_ps(inputVector);  // Load the input vector
+  __m128 row1 = _mm_loadu_ps(matrix);        // Load the first row of the matrix
+  __m128 row2 = _mm_loadu_ps(matrix + 4);    // Load the second row
+  __m128 row3 = _mm_loadu_ps(matrix + 8);    // Load the third row
+  __m128 row4 = _mm_loadu_ps(matrix + 12);   // Load the fourth row
+  __m128 input = _mm_loadu_ps(inputVector);  // Load the input vector
 
   // Perform the vector matrix multiplication
+  // Add all four element-wise products together
   __m128 result =
       _mm_add_ps(_mm_add_ps(_mm_mul_ps(row1, input), _mm_mul_ps(row2, input)),
                  _mm_add_ps(_mm_mul_ps(row3, input), _mm_mul_ps(row4, input)));
 
   // Store the result back into memory
-  _mm_store_ps(resultVector, result);
+  _mm_storeu_ps(resultVector, result);
 }
 
 int main() {
